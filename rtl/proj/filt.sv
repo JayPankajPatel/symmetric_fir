dd723b756a2dcf45f7d93557550ba5f9388145cf
JayPankajPatel <jayp102501@gmail.com>
2025-05-07 18:06:07 -0700

    COMPLETED: target 512MHz met and passed all simulations

--- rtl/proj/filt.sv
+++ rtl/proj/filt.sv
@@ -1,4 +1,4 @@
-//`include "counter.sv"
+
 `include "fircoefs2.v"
 
 module filt (
@@ -14,18 +14,6 @@ module filt (
     typedef enum logic [2:0] {IDLE, LOAD, CALC, OUT} state_t;
     state_t state, next_state;
 
-    
-    //logic [8:0] buf_idx; // 9 bits needed for 0–511
-    //logic [8:0] calc_buf_idx; // 9 bits needed for 0–511
-    
-
-    //logic en_calc_buf_idx; 
-    //logic calc_end_of_buffer; 
-
-    //assign rst_calc_buf_idx = (state == LOAD); 
-    //assign en_calc_buf_idx = (state == CALC); 
-    //assign calc_end_of_buffer = (calc_buf_idx == 9'd511); 
-
     logic [511:0] buf_in_d, buf_in;
     logic [511:0] buf_calc_d, buf_calc;
     logic signed [35:0] acc, acc_d; 
@@ -40,8 +28,6 @@ module filt (
     assign buf_calc = FILTER ? buf_in_d : buf_calc_d; 
 
     assign h = coef(counter_d);
-
-
     //buffer logic start
 
     always_ff @(posedge Clock or posedge Reset) begin
@@ -62,6 +48,7 @@ module filt (
     end
     //buffer logic end
 
+    // control path start
     always_ff @(posedge Clock or posedge Reset) begin
         if(Reset) begin 
             state <= IDLE; 
@@ -71,7 +58,6 @@ module filt (
         end 
     end
 
-    // control path
     always_comb begin
         next_state = state;
         case (state)
@@ -97,9 +83,10 @@ module filt (
             end
         endcase
     end
+    // control path end
 
 
-    // data path for calculation and output
+    // data path start
     always_ff @(posedge Clock or posedge Reset) begin 
         if(Reset) begin 
             push_d <= 0; 
@@ -163,34 +150,10 @@ module filt (
             end
         endcase 
     end
-
+    // data path end
+    
     assign Push = push_d;
     assign Dout = dout_d;
 
 
-///    always_ff @(posedge Clock or posedge Reset) begin 
-///        if(Reset) begin 
-///            state <= IDLE; 
-///            Push <= 0; 
-///            Dout <= 0; 
-///            acc <= 0; 
-///        end
-///        else begin 
-///            state <= next_state; 
-///            case (state)  
-///                CALC: begin 
-///                    case({buf_calc[calc_buf_idx], buf_calc[511-calc_buf_idx]})
-///                        2'b00: acc <= acc; 
-///                        2'b01, 2'b10: acc <= acc + h; 
-///                        2'b11: acc <= acc + (h <<< 1); // mulitplying by 2
-///                    endcase
-///                end
-///                OUT: begin 
-///                    Dout <= (acc + 36'sd128) >>> 8;  // rounding step
-///                    Push <= '1;
-///                    acc <= 0; 
-///                end
-///            endcase
-///        end
-///    end
 endmodule : filt
